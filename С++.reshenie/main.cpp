f39485adebabb7d1d73fbb74b528f99f22628892
kolbinglebvadimovich <sonya54842@yandex.ru>
2026-02-25 22:08:23 +0700

    Update main.cpp

--- С++.reshenie/main.cpp
+++ С++.reshenie/main.cpp
@@ -2,19 +2,21 @@
 
 int main()
 {
-   const int size[5];
+   int size { 5 };
    int integers[size]{ 1, 2, 3, 4, 5 };
 
-   for (int element{}; element < size; ++element)
+   int array2[size];
+   array2[0] = 1000;
+   array2[1] = 2000;
+   array2[2] = 3000;
+   array2[3] = 4000;
+   array2[4] = 4000;
+
+   for ( int element{}; element < size; ++element)
    {
        std::cout << "Element[" << element << "]: " << array2[element] << std::endl;
    }
-   
-
-   return EXIT_SUCCESS;
 }
 
 
-
-
-
+    return EXIT_SUCCESS;
